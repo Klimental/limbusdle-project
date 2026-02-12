0c57967abf67ac10ec032c0ac1707ce7f2c14e02
Student <student@example.com>
2026-02-12 09:35:13 +0200

    Add all Yi Sangs id to database

--- sql/02_data.sql
+++ sql/02_data.sql
@@ -13,7 +13,19 @@ INSERT INTO sinners (name) VALUES
 ('Gregor');
 
 INSERT INTO identities (sinner_id, name, rarity, season, faction, skill1, skill2, skill3, defense, image_path) VALUES 
-(1, 'W Corp. L3 Cleanup Agent', '000', 'Season 1', 'W Corp', 'Slash', 'Slash', 'Slash', 'Evade', 'img/yi_sang_w.png'),
-(2, 'N Corp. The One Who Grips', '000', 'Season 2', 'N Corp', 'Blunt', 'Pierce', 'Blunt', 'Guard', 'img/faust_n.png'),
-(7, 'R Corp. 4th Pack Rabbit', '000', 'Season 1', 'R Corp', 'Pierce', 'Pierce', 'Pierce', 'Evade', 'img/heathcliff_r.png'),
-(10, 'LCB Sinner', '0', 'Base', 'LCB', 'Slash', 'Slash', 'Slash', 'Guard', 'img/sinclair_base.png');
+-- Yi Sang (1)--
+(1, 'LCB Sinner', '0', 'Season 0', 'Limbus Company LCB', 'Slash', 'Pierce', 'Slash', 'Guard', 'img/yi_sang_base.png'),
+(1, 'Seven Assoc. South Section 6', '00', 'Season 0', 'Seven Association', 'Pierce', 'Pierce', 'Blunt', 'Guard', 'img/yi_sang_seven.png'),
+(1, 'Blade Lineage Salsu', '000', 'Season 0', 'Blade Lineage', 'Slash', 'Slash', 'Slash', 'Counter', 'img/yi_sang_blade.png'),
+(1, 'Effloresced E.G.O::Spicebush', '000', 'Season 2', 'Technology Liberation Alliance', 'Pierce', 'Blunt', 'Pierce', 'Evade', 'img/yi_sang_spicebush.png'),
+(1, 'Molar Office Fixer', '00', 'Season 2', 'Molar Office', 'Pierce', 'Blunt', 'Blunt', 'Guard', 'img/yi_sang_molar.png'),
+(1, 'W Corp. L3 Cleanup Agent', '000', 'Season 0', 'W Corp.', 'Slash', 'Pierce', 'Pierce', 'Evade', 'img/yi_sang_w.png'),
+(1, 'The Pequod First Mate', '00', 'Season 3', 'The Pequod', 'Pierce', 'Pierce', 'Pierce', 'Evade', 'img/yi_sang_pequod.png'),
+(1, 'Dieci Assoc. South Section 4', '00', 'Season 0', 'Dieci Association', 'Pierce', 'Blunt', 'Blunt', 'Guard', 'img/yi_sang_dieci.png'),
+(1, 'The Ring Pointillist Student', '000', 'Season 0', 'The Ring', 'Pierce', 'Pierce', 'Pierce', 'Guard', 'img/yi_sang_ring.png'),
+(1, 'Lobotomy E.G.O::Solemn Lament', '000', 'Walpurgis Night', 'L Corp.', 'Pierce', 'Pierce', 'Pierce', 'Guard', 'img/yi_sang_solemn.png'),
+(1, 'LCB E.G.O::Lantern', '00', 'Season 5', 'Limbus Company LCE', 'Blunt', 'Pierce', 'Blunt', 'Counter', 'img/yi_sang_lantern.png'),
+(1, 'Liu Assoc. South Section 3', '000', 'Season 0', 'Liu Association', 'Slash', 'Slash', 'Slash', 'Counter', 'img/yi_sang_liu.png'),
+(1, 'N Corp. E.G.O::Fell Bullet', '000', 'Season 6', 'N Corp.', 'Blunt', 'Pierce', 'Pierce', 'Clashable Counter', 'img/yi_sang_fell.png'),
+(1, 'Heishou Pack - Wu Branch Adept', '000', 'Season 6', 'H Corp.', 'Slash', 'Slash', 'Slash', 'Clashable Guard', 'img/yi_sang_heishou.png');
+--Faust (2)--
